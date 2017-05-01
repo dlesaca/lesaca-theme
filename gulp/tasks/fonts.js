9a796e7b3e3cebf8b89121f0210d84bf58327cd3
Dexter Lesaca <dlesaca@us.ibm.com>
2017-05-01 14:18:34 -0700

    add bg image and fix gulp image task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 //Builds fonts
 gulp.task('fonts', () => {
   return gulp.src('./src/fonts/**/*')
-  .pipe(gulp.dest('./public/assets/css/fonts'));
+  .pipe(gulp.dest('./public/assets/fonts'));
 });
